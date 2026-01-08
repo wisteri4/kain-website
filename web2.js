9058496a1c0546392d59d2c3b5d6bf0aea8c8c3e
Izabel <165130557+wisteri4@users.noreply.github.com>
2026-01-09 00:49:07 +0400

    Delete web2.js

--- web2.js
+++
@@ -1,9 +0,0 @@
-const { MongoClient } = require('mongodb'); 
-const connectionString = 'mongodb://localhost:27017';
-const client = new MongoClient(connectionString);
-
-async function connectToDatabase(){
-    await client.connect();
-    console.log('Connected to MongoDB');
-}
-connectToDatabase();
